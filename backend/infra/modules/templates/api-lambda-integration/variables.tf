98b61ba88e6339047265ea22ce1150dec2d3ad23
Maitra Patel <10maitra22@gmail.com>
2025-11-13 02:37:20 -0700

    feat + template for individual API endpoint resource for independent creation ~ variables to match this change

--- backend/infra/modules/templates/api-lambda-integration/variables.tf
+++ backend/infra/modules/templates/api-lambda-integration/variables.tf
@@ -1,54 +1,48 @@
-variable "path_part" {
-  description = "API endpoint path (ex. log-in)"
-  type        = string
-}
-
-variable "model_name" {
-  description = "Name of the API Gateway model"
-  type        = string
-}
-
-variable "description" {
-  description = "Description of the model"
-  type        = string
-}
-
-variable "schema" {
-  description = "JSON schema for validation"
-  type        = string
-}
-
-variable "func_name" {
-  description = "Lambda function name"
-  type        = string
-}
-
-variable "invoke_arn" {
-  description = "Lambda invoke ARN"
-  type        = string
-}
-
 variable "rest_api_id" {
   description = "Parent API Gateway ID"
   type        = string
 }
-
-variable "execution_arn" {
-  description = "Execution ARN of the REST API"
+variable "model_name" {
+  description = "Name of the API Gateway model"
   type        = string
 }
-
+variable "description" {
+  description = "Description of the model"
+  type        = string
+}
+variable "schema" {
+  description = "JSON schema for validation"
+  type        = string
+}
+variable "resource_id" {
+  description = "The ID of the path resource that will be the endpoint path itself (ex. /endpoint, /diagram/create)"
+  type        = string
+}
+variable "http_method" {
+  description = "HTTP method for the API Gateway method (e.g., GET, POST, PUT, DELETE, UPDATE)"
+  type        = string
+}
 variable "request_validator_id" {
   description = "Validator ID for request body validation"
   type        = string
 }
-
-variable "http_method" {
-  description = "HTTP method for the API Gateway method (e.g., GET, POST, PUT, DELETE)"
+variable "invoke_arn" {
+  description = "Lambda invoke ARN"
   type        = string
 }
-
-variable "root_resource_id" {
-  description = "The root resource ID of the REST API"
+variable "func_name" {
+  description = "Lambda function name"
   type        = string
+}
+variable "path_part" {
+  description = "API endpoint path name (ex. log-in)"
+  type        = string
+}
+variable "execution_arn" {
+  description = "Execution ARN of the REST API"
+  type        = string
+}
+variable "path" {
+  description = "The full path of the API resource (e.g., /diagram or /parent/child)"
+  type = string
 }
---
+++ backend/infra/modules/templates/api-path/main.tf
@@ -0,0 +1,7 @@
+resource "aws_api_gateway_resource" "path" {
+
+    rest_api_id = var.rest_api_id
+    parent_id   = var.parent_id
+    path_part   = var.path_part
+
+}
