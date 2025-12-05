453e69f2b84234dc3acf57915070e64edd94ba25
Maitra Patel <10maitra22@gmail.com>
2025-12-05 03:50:13 -0700

    feat + organization creation body validation model + request data input retrieval for organization name + authorizer context processing to get user data (generic function and struct to be used by all other functions requiring user data)

--- backend/api/organization/create/main.go
+++ backend/api/organization/create/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/Alberta2514640/clutter/backend/api/generic"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,6 +15,29 @@ func main() {
 
 }
 
+type requestBody struct {
+	OrganizationName string `json:"organizationName"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return generic.Response(200, generic.Json{"message": "organization-create Lambda is working!"})
+
+	// The authorizer context retains data about the user extracted from the JWT inside the authorizer Lambda function
+	userData, err := generic.GetUserDataFromAuthorizerContext(request.RequestContext.Authorizer)
+	if err != nil {
+		return generic.Response(
+			http.StatusInternalServerError,
+			generic.Json{"message": "Failed to retrieve user data from autorizer context", "error": err.Error()},
+		)
+	}
+
+	// Process body from API Gateway request
+	var body requestBody
+
+	err = json.Unmarshal([]byte(request.Body), &body)
+	if err != nil {
+		return generic.Response(http.StatusBadRequest, generic.Json{"message": "Bad Request", "error": err.Error()})
+	}
+	organizationName := body.OrganizationName
+
+	return generic.Response(200, generic.Json{"message": "organization-create Lambda is working!", "organizationName": organizationName, "userData": userData})
 }
