7d1dd96ad7efbc1eb9140ecc337e2297b9871053
Santiago Fuentes <santiago@fuentes.it.com>
2025-12-12 12:19:20 -0700

    change  + remove console log  +  remove packages

--- frontend/clutter/src/app/(diagram)/projects/[projectId]/diagram/[diagramId]/_components/DiagramEditor.tsx
+++ frontend/clutter/src/app/(diagram)/projects/[projectId]/diagram/[diagramId]/_components/DiagramEditor.tsx
@@ -32,8 +32,6 @@ export default function DiagramEditor({
     loadDiagram 
   } = useDiagramActions();
 
-  console.log(nodes, edges, dirty);
-
   useEffect(() => {
     setContext(projectId, diagramId);
     loadDiagram(projectId, diagramId);
