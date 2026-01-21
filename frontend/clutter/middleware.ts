89da830f991a5ad22f2f47c58418fb636d77a690
Nimna Devinda <nimnawijedasa@gmail.com>
2026-01-21 12:02:51 -0700

    removing next auth

--- frontend/clutter/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  // matcher: ["/dashboard/:path*", "/projects/:path*", "/workspaces/:path*", "/runs/:path*"],
-  matcher: [],
-};
