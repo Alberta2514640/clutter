c4bea6f85f28bacaad321cd796a0c18755b3278f
Maitra Patel <10maitra22@gmail.com>
2026-01-26 13:18:08 -0700

    feat + new s3 for templates

--- backend/infra/modules/s3/main.tf
+++ backend/infra/modules/s3/main.tf
@@ -2,6 +2,11 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
+# ================================================================================================================================
+# Bucket - Clutter
+# Private
+# Description: Will house user diagram generated Terraform code and Ansible playbooks
+# ================================================================================================================================
 resource "aws_s3_bucket" "clutter_bucket" {
   bucket = "clutter-${var.aws_region}-${random_id.suffix.hex}"
 }
@@ -82,7 +87,7 @@ resource "aws_s3_bucket_policy" "clutter_bucket" {
         Resource  = "${aws_s3_bucket.clutter_bucket.arn}/*"
         Condition = {
           StringNotEquals = {
-            "s3:x-amz-server-side-encryption" = var.kms_key_arn != null ? "aws:kms" : "AES256"
+            "s3:x-amz-server-side-encryption": "AES256"
           }
         }
       }
@@ -97,4 +102,61 @@ resource "aws_s3_bucket_ownership_controls" "clutter_bucket" {
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
+}
+
+# ================================================================================================================================
+# Bucket - Templates
+# Public
+# Description: Will house generic Terraformr esource templates for use by the generator and other public templates
+# ================================================================================================================================
+resource "aws_s3_bucket" "templates" {
+  bucket = "clutter-templates-${var.aws_region}-${random_id.suffix.hex}"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "templates_encryption" {
+  bucket = aws_s3_bucket.templates.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "templates_public_access" {
+  bucket = aws_s3_bucket.templates.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "templates_policy" {
+  bucket = aws_s3_bucket.templates.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.templates.arn}/*"
+    }]
+  })
+}
+
+
+# Object - Client-side Terraform Deployer Role Template
+resource "aws_s3_object" "terraform_deployer_role_template" {
+
+  bucket = aws_s3_bucket.templates.id
+
+  key = "templates/cloudformation/client_side_terraform_deployer_role.yaml"
+  source = "${path.module}/uploads/client_side_terraform_deployer_role.yaml"
+
+  server_side_encryption = "AES256"
+
+  etag = filemd5("${path.module}/uploads/client_side_terraform_deployer_role.yaml")
+
 }
