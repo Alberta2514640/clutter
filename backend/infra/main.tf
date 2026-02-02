72a2812b695d3ac0ae0b2d57e0521059c85f5e85
Hamza Amar <rhamzax@gmail.com>
2026-02-01 17:15:34 -0700

    feat + Useing stage name variable in main.tf (Ignore)

--- backend/infra/main.tf
+++ backend/infra/main.tf
@@ -826,5 +826,5 @@ variable "stage_name" {
 resource "aws_api_gateway_stage" "clutter" {
   rest_api_id   = module.clutter-api-gateway.rest_api_id
   deployment_id = aws_api_gateway_deployment.clutter.id
-  stage_name    = "dev"
+  stage_name    = var.stage_name
 }
