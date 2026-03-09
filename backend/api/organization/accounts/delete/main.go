50abd2b5dc2854850a8adf62696d45ec6afe5fd5
Maitra Patel <10maitra22@gmail.com>
2026-03-09 06:43:58 -0600

    feat + initial infra for client aws account role helper functions

---
+++ backend/api/organization/accounts/delete/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Alberta2514640/clutter/backend/api/generic"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return generic.Response(http.StatusOK, generic.Json{
+		"message": "deleted organization account test",
+	})
+}
