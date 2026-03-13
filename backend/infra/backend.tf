79c9046f76c0d6da889d45629258e82deeff2bbe
Maitra Patel <10maitra22@gmail.com>
2026-03-13 16:03:56 -0600

    feat + model validation + initial checks on params

--- backend/infra/backend.tf
+++ backend/infra/backend.tf
@@ -1,7 +1,17 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "tfstate-us-west-2-5727f4ce"
+#     key            = "clutter/infra/terraform.tfstate"
+#     region         = "us-west-2"
+#     dynamodb_table = "tflock"
+#     encrypt        = true
+#   }
+# }
+
 terraform {
   backend "s3" {
-    bucket         = "tfstate-us-west-2-5727f4ce"
-    key            = "clutter/infra/terraform.tfstate"
+    bucket         = "tfstate-us-west-2-7341aab7"
+    key            = "clutter/infra/developer/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "tflock"
     encrypt        = true
