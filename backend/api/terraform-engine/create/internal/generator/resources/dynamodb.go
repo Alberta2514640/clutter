a3448698ee17510867928aab76e2e287ee31758a
Hamza Amar <rhamzax@gmail.com>
2026-03-24 18:18:02 -0600

    feat + Add DynamoDB translations

--- backend/api/terraform-engine/create/internal/generator/resources/dynamodb.go
+++ backend/api/terraform-engine/create/internal/generator/resources/dynamodb.go
@@ -1,19 +1,27 @@
 package resources
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/Alberta2514640/clutter/backend/api/generic"
+	"github.com/Alberta2514640/clutter/backend/api/terraform-engine/create/internal/generator/template"
 )
 
-type DynamoDBGenerator struct{}
+type DynamoDBGenerator struct {
+	templateLoader *template.TemplateLoader
+	ctx            context.Context
+}
 
-func NewDynamoDBGenerator() *DynamoDBGenerator {
-	return &DynamoDBGenerator{}
+func NewDynamoDBGenerator(ctx context.Context, loader *template.TemplateLoader) *DynamoDBGenerator {
+	return &DynamoDBGenerator{
+		templateLoader: loader,
+		ctx:            ctx,
+	}
 }
 
 func (g *DynamoDBGenerator) GetRequiredVariables() []string {
-	return []string{"table_name"}
+	return []string{"resource_name"}
 }
 
 func (g *DynamoDBGenerator) ValidateVariables(variables map[string]interface{}) error {
@@ -36,10 +44,41 @@ func (g *DynamoDBGenerator) Generate(node generic.DiagramNode, resourceName stri
 	if err := g.ValidateVariables(node.Variables); err != nil {
 		return "", err
 	}
-	return "", nil
-	
+
+	templatePath := template.GetTemplatePath("dynamodb", "main.tf.tmpl")
+	tmpl, err := g.templateLoader.Load(g.ctx, templatePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to load dynamodb template: %w", err)
+	}
+
+	vars := map[string]interface{}{
+		"ResourceName": resourceName,
+		"TableName":    generic.GetString(node.Variables, "resource_name", ""),
+		"BillingMode":  generic.GetString(node.Variables, "billing_mode", "PAY_PER_REQUEST"),
+		"HashKey":      generic.GetString(node.Variables, "hash_key", "id"),
+		"HashKeyType":  generic.GetString(node.Variables, "hash_key_type", "S"),
+		"EnableTTL":    generic.GetBool(node.Variables, "enable_ttl", false),
+		"TTLAttribute": generic.GetString(node.Variables, "ttl_attribute", "ttl"),
+	}
+
+	return template.Render(tmpl, vars)
 }
 
 func (g *DynamoDBGenerator) GetOutputs(resourceName string) string {
-	return ""
+	templatePath := template.GetTemplatePath("dynamodb", "outputs.tf.tmpl")
+	tmpl, err := g.templateLoader.Load(g.ctx, templatePath)
+	if err != nil {
+		return ""
+	}
+
+	vars := map[string]interface{}{
+		"ResourceName": resourceName,
+	}
+
+	output, err := template.Render(tmpl, vars)
+	if err != nil {
+		return ""
+	}
+
+	return output
 }
