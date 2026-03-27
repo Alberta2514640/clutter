94a2531c2d3e6529ec01225de3a56abdd3a078aa
Hamza Amar <rhamzax@gmail.com>
2026-03-27 03:39:33 -0600

    msc + Add generated files to a sub terraform folder

--- backend/api/terraform-engine/create/internal/generator/writer/writer.go
+++ backend/api/terraform-engine/create/internal/generator/writer/writer.go
@@ -55,7 +55,7 @@ func (w *TerraformWriter) WriteToS3(ctx context.Context, tf *internal.GeneratedT
 			return fmt.Errorf("missing org/project/diagram id for s3 path (org=%q project=%q diagram=%q)", tf.OrgID, tf.ProjectID, tf.DiagramID)
 		}
 
-		key := fmt.Sprintf("%s/%s/%s/%s", tf.OrgID, tf.ProjectID, tf.DiagramID, filename)
+		key := fmt.Sprintf("%s/%s/%s/terraform/%s", tf.OrgID, tf.ProjectID, tf.DiagramID, filename)
 		_, err := w.s3Client.PutObject(ctx, &s3.PutObjectInput{
 			Bucket:               aws.String(w.bucketName),
 			Key:                  aws.String(key),
