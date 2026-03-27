ed0b9ec3c796263d89e2cfa184bb5473dce2dc5e
Nimna Devinda <nimnawijedasa@gmail.com>
2026-03-27 11:50:34 -0600

    removed ansiblePlaybookId to user

--- frontend/clutter/src/app/(diagram)/projects/[projectId]/diagram/[diagramId]/_components/nodes/ConfigPanel.tsx
+++ frontend/clutter/src/app/(diagram)/projects/[projectId]/diagram/[diagramId]/_components/nodes/ConfigPanel.tsx
@@ -338,13 +338,6 @@ export default function ConfigPanel({
                       </div>
                     </div>
 
-                    <div className="rounded-lg border border-slate-800 bg-slate-950/70 px-3 py-2">
-                      <div className="text-xs text-gray-400">Playbook ID</div>
-                      <div className="mt-1 break-all text-sm font-medium text-white">
-                        {selectedNode!.data.ansiblePlaybookId ?? "Not available until upload succeeds"}
-                      </div>
-                    </div>
-
                     <button
                       type="button"
                       onClick={handleRunPlaybook}
