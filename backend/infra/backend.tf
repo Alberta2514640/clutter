c8140395d7c58b9b775afed5f790c8965bbe6726
Maitra Patel <10maitra22@gmail.com>
2026-03-31 17:34:08 -0600

    fix + backend s3

--- backend/infra/backend.tf
+++ backend/infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-us-west-2-7341aab7"
+    bucket         = "tfstate-us-west-2-5727f4ce"
     key            = "clutter/infra/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "tflock"
